4afd3ffceacb3000fe7321c40deb5dd3aa746a5b
Igor Lukanin <me@igorlukanin.io>
2020-07-02 18:40:01 +0300

    Commit vendored dependencies

--- app/main.rs
+++ app/main.rs
@@ -1,7 +1,10 @@
+use http_body::Body as _;
+use hyper::{Client, Request, Method, Body, StatusCode};
 use std::env;
-use std::io::Error;
+use std::process;
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let args: Vec<String> = env::args().collect();
 
     let server_url = &args[1];
@@ -9,8 +12,43 @@ fn main() -> Result<(), Error> {
 
     println!("ServerUrl: {}; PlayerKey: {}", server_url, player_key);
 
-    let response = isahc::get(format!("{}?player_key={}", server_url, player_key))?;
-    assert!(response.status().is_success());
+    let client = Client::new();
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(server_url)
+        .body(Body::from(format!("{}", player_key)))?;
+
+    match client.request(req).await {
+        Ok(mut res) => {
+            match res.status() {
+                StatusCode::OK => {
+                    print!("Server response: ");
+                    while let Some(chunk) = res.body_mut().data().await {
+                        match chunk {
+                            Ok(content) => println!("{:?}", content),
+                            Err(why) => println!("error reading body: {:?}", why)
+                        }
+                    }
+                },
+                _ => {
+                    println!("Unexpected server response:");
+                    println!("HTTP code: {}", res.status());
+                    print!("Response body: ");
+                    while let Some(chunk) = res.body_mut().data().await {
+                        match chunk {
+                            Ok(content) => println!("{:?}", content),
+                            Err(why) => println!("error reading body: {:?}", why)
+                        }
+                    }
+                    process::exit(2);
+                }
+            }
+        },
+        Err(err) => {
+            println!("Unexpected server response:\n{}", err);
+            process::exit(1);
+        }
+    }
 
     Ok(())
 }
